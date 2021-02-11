8cf95cb8de6a920aace5397d33ca40c029b3361b
rolojf <rolo@tutamail.com>
2021-02-11 01:02:20 -0600

    Mostrar menú movil cuando click en ícono y al reves

--- src/Shared.elm
+++ src/Shared.elm
@@ -360,7 +360,7 @@ myNav modelo =
                 , mobileMenuButton modelo
                 ]
             ]
-        , myHiddenMenu
+        , myHiddenMenu modelo
         ]
 
 
@@ -450,10 +450,23 @@ myHiddenBlock =
         ]
 
 
-myHiddenMenu : Html msg
-myHiddenMenu =
+
+-- Mobile menu, toggle classes based on menu state.
+-- Open: "block", closed: "hidden"
+
+
+myHiddenMenu : Model -> Html msg
+myHiddenMenu modelo =
     div
-        [ class "hidden md:hidden" ]
+        [ class <|
+            "md:hidden "
+                ++ (if modelo.showMobileMenu then
+                        "block"
+
+                    else
+                        "hidden"
+                   )
+        ]
         [ div
             [ class "px-2 pt-2 pb-3 space-y-1 sm:px-3" ]
             -- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --
